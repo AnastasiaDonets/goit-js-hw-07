3e6e20735f27d7c373a0e4c4166ddd3ef9138d48
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-03-05 00:08:19 +0200

    Task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItem = galleryItems
+  .map(item => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></li>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
